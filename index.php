ede76b93e906ead2f95c2a49f80df8a74ee8f02e
ahmedsammygeek <ahmedsamigeek@gmail.com>
2015-07-30 18:17:46 +0200

    add the index to salim repo

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+$name = "ahmed salim";
+$age = 22;
+
+// the normal way to echo data
+echo "my name is $name &<br> my age is $age <br>";
+
+// other way to echo in the screen with vars
+echo "my name is :".$name." & my age is ".$age  ;
+
+ ?>
