8d56ba63bb3c4d2d09b09638f835c579071fdd64
ahmedsammygeek <ahmedsamigeek@gmail.com>
2015-07-30 18:54:05 +0200

    check

--- index.php
+++ index.php
@@ -1,63 +1,78 @@
 <?php 
 
 
-$name = "ahmed salim";
-$age = 22;
+// $name = "ahmed salim";
+// $age = 22;
 
-// the normal way to echo data
-echo "my name is $name &<br> my age is $age <br>";
+// // the normal way to echo data
+// echo "my name is $name &<br> my age is $age <br>";
 
-// other way to echo in the screen with vars
-echo "my name is :".$name." & my age is ".$age  ;
+// // other way to echo in the screen with vars
+// echo "my name is :".$name." & my age is ".$age  ;
 
 
 
 // the data types
 // $name = "ahmed sami is ust a normal deveoper"; // string 
-$age = 22 ; // the data here is intger
-$bolean = true ; // the data here is boolean
-$bolean = false;
+// $age = 22 ; // the data here is intger
+// $bolean = true ; // the data here is boolean
+// $bolean = false;
 
 // $name = "ahmed sami";
 
-echo "my name is:".$name;
+// echo "my name is:".$name;
 
 // echo "<br>";
 
-$name = "ahmed salim";
+// $name = "ahmed salim";
 
-if($age > 21 && $name == "ahmed Salim") {
-	echo "<br><p>welcome</p>";
-}
-else {
-	echo '<br><p>that\'s not you </p>';
-}
+// if($age > 21 && $name == "ahmed Salim") {
+// 	echo "<br><p>welcome</p>";
+// }
+// else {
+	// echo '<br><p>that\'s not you </p>';
+// }
 
 
 // the differnce between == and ===
 
-echo "<br>";
-$num1 = 22;
-$num2 = "22";
+// echo "<br>";
+// $num1 = 22;
+// $num2 = "22";
 
-if($num2 === $num1) {
-	echo "equal";
+// if($num2 === $num1) {
+// 	echo "equal";
+// }
+// else {
+// 	echo "not equal";
+// }
+
+
+// echo "<br>";
+// if($num1 == 22) {
+// echo "it's euqsl 22";
+// }
+// elseif($num1 == 23) {
+// echo "it's euqsl 23";
+// }
+
+// elseif($num1 == 24) {
+// echo "it's euqsl 24";
+// }
+
+
+
+// 5od balk men de awe mohema 
+$var = 3;
+$var2 = 6;
+if( ($var == 3 || $var == 4) && $var2 == 6  ) 
+{
+	echo "yes";
 }
 else {
-	echo "not equal";
+	echo "no";
 }
 
 
-echo "<br>";
-if($num1 == 22) {
-echo "it's euqsl 22";
-}
-elseif($num1 == 23) {
-echo "it's euqsl 23";
-}
-
-elseif($num1 == 24) {
-echo "it's euqsl 24";
-}
 
 ?>
