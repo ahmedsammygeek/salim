c13d8e1c41d32d0e6b4aa25d9b88b09e41e5a1e0
ahmedsammygeek <ahmedsamigeek@gmail.com>
2015-07-30 18:48:06 +0200

    another code

--- index.php
+++ index.php
@@ -10,4 +10,54 @@ echo "my name is $name &<br> my age is $age <br>";
 // other way to echo in the screen with vars
 echo "my name is :".$name." & my age is ".$age  ;
 
- ?>
+
+
+// the data types
+// $name = "ahmed sami is ust a normal deveoper"; // string 
+$age = 22 ; // the data here is intger
+$bolean = true ; // the data here is boolean
+$bolean = false;
+
+// $name = "ahmed sami";
+
+echo "my name is:".$name;
+
+// echo "<br>";
+
+$name = "ahmed salim";
+
+if($age > 21 && $name == "ahmed Salim") {
+	echo "<br><p>welcome</p>";
+}
+else {
+	echo '<br><p>that\'s not you </p>';
+}
+
+
+// the differnce between == and ===
+
+echo "<br>";
+$num1 = 22;
+$num2 = "22";
+
+if($num2 === $num1) {
+	echo "equal";
+}
+else {
+	echo "not equal";
+}
+
+
+echo "<br>";
+if($num1 == 22) {
+echo "it's euqsl 22";
+}
+elseif($num1 == 23) {
+echo "it's euqsl 23";
+}
+
+elseif($num1 == 24) {
+echo "it's euqsl 24";
+}
+
+?>
