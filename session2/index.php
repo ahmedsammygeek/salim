79b43a3bc88fbe32b18874062520a48347b39e8f
ahmedsammygeek <ahmedsamigeek@gmail.com>
2015-08-09 18:32:29 +0200

    session 2

---
+++ session2/index.php
@@ -0,0 +1,59 @@
+<?php 
+
+//if
+//switch
+//while
+//
+
+
+
+// for($a=0;$a < 4 ;$a++) {
+// 	echo "ahmed<br>";
+// }
+
+
+// echo $a ; // it will echo 4 cuz the final number stored in the $a is 4
+
+// $name = "ahmed , ali , moahmed";
+// echo $name ;
+// echo "<br>" ;
+// $name = "ahmed";
+// $name = "ali";
+// $name = "moahmed";
+// echo $name ;
+
+$names = array("ahmed" , "ali" , "salim");
+$user_info = array("name" => "ali"  , "age" => 22 , "city"=>"mansoura" , 90 , "email" =>"ahmed@yahoo.com" , "ay comment" );
+
+// echo $names[0];
+// echo $user_info["city"];
+// echo $user_info[1];
+
+
+
+// $users = array( array("name" => "ahmed" ,  array("final" => array(22 , 34)),  "age"=>33  , array("salim")));
+
+// echo $users[0][0]["final"][1];
+
+$ages = array(11 , "name" => "sdsd" , 33 , 44 , 55);
+
+// echo $ages[0] + $ages[1];
+
+// for ($i=0; $i <  ; $i++) { 
+// 	echo $ages[$i];
+// 	echo "<br>";
+// }
+
+// foreach($ages as $age = sdsd){
+// echo $age;
+// echo "<br>";
+// }
+
+
+// var_dump($ages);
+// $ages["myname"] = "ahmed samy";
+
+// var_dump($ages);
+
+
+?>
